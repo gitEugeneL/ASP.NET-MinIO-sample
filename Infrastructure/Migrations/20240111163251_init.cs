7d3c318a54ff3c32784a4c8029444199e99469b9
Eugene L. <euglihon.dev@gmail.com>
2024-01-15 16:10:46 +0100

    added initial migration

---
+++ Infrastructure/Migrations/20240111163251_init.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BucketName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    FileName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    FileType = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    FileSize = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    LastModified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileData", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileData_FileName",
+                table: "FileData",
+                column: "FileName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileData");
+        }
+    }
+}
